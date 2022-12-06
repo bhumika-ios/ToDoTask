d2434934c6b4a5390238d077ed3eba1c712deb10
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 12:02:29 +0530

    set Hstack - section under form

--- ToDoTask/View/CreateTask.swift
+++ ToDoTask/View/CreateTask.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct CreateTask: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form{
+            Section{
+                HStack{
+                    
+                }
+            }
+        }
     }
 }
 
