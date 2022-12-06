0f0df97dd06ae3c57ff19e9dd08f660b4ce662d8
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 12:34:34 +0530

    set object and tag in pickerselection

--- ToDoTask/View/ReminderView.swift
+++ ToDoTask/View/ReminderView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 struct ReminderView: View {
     //define picker objject
-   // @Binding var selectedPicker:
+    @Binding var selectedPicker: ReminderType
+    
     var body: some View {
         VStack{
-            Picker("Notification Trigger"){
-                Text("Interval")
-                Text("Date")
+            Picker("Notification Trigger", selection: $selectedPicker){
+                Text("Interval").tag(ReminderType.time)
+                Text("Date").tag(ReminderType.calender)
             }
+            
         }
     }
 }
 
-struct ReminderView_Previews: PreviewProvider {
-    static var previews: some View {
-        ReminderView()
-    }
-}
+//struct ReminderView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ReminderView()
+//    }
+//}
