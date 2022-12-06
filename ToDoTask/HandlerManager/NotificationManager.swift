1e7621836ef2e249c4b461c8cd48913e3a1d0aa8
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 14:05:34 +0530

    create requestauthorize function

--- ToDoTask/HandlerManager/NotificationManager.swift
+++ ToDoTask/HandlerManager/NotificationManager.swift
@@ -7,14 +7,20 @@
 
 import Foundation
 import UserNotifications
-
+// enum define object time and calendar
 enum NotificationManagerConstants {
     static let timeBaseNotification = "TimeBaseNotification"
     static let calendarBaseNotification = "CalendarBaseNotification"
 }
 
 class NotificationManager: ObservableObject {
+    // define object notificationmanager
     static let shared = NotificationManager()
-    
-    
+    // authorize notification call
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { greanted, _ in
+                
+            }
+    }
 }
