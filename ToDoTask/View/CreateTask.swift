e5e609d5af78853a37a4459f08dc49927c39d186
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 12:00:48 +0530

    create - createTask file

---
+++ ToDoTask/View/CreateTask.swift
@@ -0,0 +1,20 @@
+//
+//  CreateTask.swift
+//  ToDoTask
+//
+//  Created by Bhumika Patel on 06/12/22.
+//
+
+import SwiftUI
+
+struct CreateTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateTask_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateTask()
+    }
+}
