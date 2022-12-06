faa2a807b5240f14f89df1eaade08d7fadeaaeb4
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 14:31:37 +0530

    json decode data for loaded data on loaded function

--- ToDoTask/HandlerManager/TaskNotificationManager.swift
+++ ToDoTask/HandlerManager/TaskNotificationManager.swift
@@ -27,6 +27,18 @@ class TaskNotificationManager {
           }
         
     }
+    func loadTasks() -> [TaskModel] {
+      let documentsDirectory = getDocumentsDirectory()
+      let storageURL = documentsDirectory.appendingPathComponent(FileConstants.tasksFileName)
+      guard
+        let taskData = try? Data(contentsOf: storageURL),
+        let tasks = try? JSONDecoder().decode([TaskModel].self, from: taskData)
+      else {
+        return []
+      }
+
+      return tasks
+    }
     func getDocumentsDirectory() -> URL {
       let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
       return paths[0]
