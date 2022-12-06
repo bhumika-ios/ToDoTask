b8d690bd26485481180f72883d22d67a5f9febbc
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 13:56:40 +0530

    create group - handler manager - create notification manager- import usernoftification - call enum value set

---
+++ ToDoTask/HandlerManager/NotificationManager.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationManager.swift
+//  ToDoTask
+//
+//  Created by Bhumika Patel on 06/12/22.
+//
+
+import Foundation
+import UserNotifications
+
+enum NotificationManagerConstants {
+    static let timeBaseNotification = "TimeBaseNotification"
+    static let calendarBaseNotification = "CalendarBaseNotification"
+}
