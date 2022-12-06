3c7ac62feffa3b7fe25c6e867b356aaddbf29bd4
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-06 11:13:38 +0530

    Initial Commit

---
+++ ToDoTask/ToDoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTaskApp.swift
+//  ToDoTask
+//
+//  Created by Bhumika Patel on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
